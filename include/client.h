be743aa0100b94c64af1d4a31492331c7b4e0a82
benjamin whalley <benjaminjwhalley@outlook.com>
2025-04-15 20:34:31 +0100

    chore@client: renamed some parameters

--- include/client.h
+++ include/client.h
@@ -6,8 +6,8 @@ class Client : public ConnectionBase {
 private:
 public:
     Client(const std::string& host, const unsigned int port) : ConnectionBase(host, port) {}
-    ~Client() = default;
+    ~Client() override = default;
     int start();
-    int send_msg(const SOCKET reciever_socket, const char message[]) override;
-    int receiver(SOCKET socket);
+    int send_msg(const SOCKET receiver_socket, const char message[]) override;
+    int receiver() const;
 };
