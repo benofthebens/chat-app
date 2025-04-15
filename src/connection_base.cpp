09cfc1cd5284d168e0b09ac2d93c7d66beeb9afa
benjamin whalley <benjaminjwhalley@outlook.com>
2025-04-15 20:55:15 +0100

    chore@connection_base: changed instance variable names

--- src/connection_base.cpp
+++ src/connection_base.cpp
@@ -1,20 +1,22 @@
-#include "../include/connection_base.h"
+#include "connection_base.h"
 #include <WinSock2.h>
 #include <string>
 #include <stdexcept>
 
-ConnectionBase::ConnectionBase(const std::string& host, const unsigned int port) : host(host), port(port) {
+ConnectionBase::ConnectionBase(const std::string& host, const unsigned int port) : host_(host), port_(port) {
     // Setup WSA
     WORD w_version_requested = MAKEWORD(2, 2); // gets the version requested
-    if (WSAStartup(w_version_requested, &this->wsa_data) != 0)
+    if (WSAStartup(w_version_requested, &this->wsa_data_) != 0)
         throw std::runtime_error("Unable to find dll");
 
     // Setup Socket
-    this->connection_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (this->connection_socket == INVALID_SOCKET) 
+    connection_socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (connection_socket_ == INVALID_SOCKET) 
         throw std::runtime_error("Unable to setup socket");
 
-    this->service.sin_family = AF_INET;
-    this->service.sin_addr.s_addr = inet_addr(host.c_str());
-    this->service.sin_port = htons(port);
+    
+    // Set up the service_
+    service_.sin_family = AF_INET;
+    service_.sin_addr.s_addr = inet_addr(host.c_str());
+    service_.sin_port = htons(port);
 }
