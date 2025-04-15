d571ac292bd75ceeba44ac3c0c99c266aad05b92
benjamin whalley <benjaminjwhalley@outlook.com>
2025-04-15 20:42:20 +0100

    chore@client: corrected spelling mistakes and updated params

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,13 +1,12 @@
-#include "../../include/client.h"
+#include "client.h"
 #include <iostream>
 #include <thread>
-#include <array>
 
 int Client::start() {
     
-    // Connect to the connection_socket with the service
-    if (connect(connection_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-        std::cout << "Failed to conenct" << std::endl;
+    // Connect to the connection_socket_ with the service_
+    if (connect(connection_socket_, reinterpret_cast<SOCKADDR*>(&service_), sizeof(service_)) == SOCKET_ERROR) {
+        std::cout << "Failed to connect" << std::endl;
         return -1;
     }
 
@@ -15,21 +14,19 @@ int Client::start() {
         // Create buffer with 200 size to get message
         char buffer[200];
 
-        // Create receiver thread to get response from server
-        std::thread receiver(&Client::receiver, this, connection_socket);
+        // Create receiver thread to get responses from server
+        std::thread receiver(&Client::receiver, this);
         receiver.detach(); // detach
 
         // Read message from cli
         std::cin.getline(buffer, 200);
-        send_msg(connection_socket, buffer);
+        send_msg(connection_socket_, buffer);
     }
-
-    return 0;
 }
 
-int Client::send_msg(const SOCKET reciever_socket, const char message[]) {
+int Client::send_msg(const SOCKET receiver_socket, const char message[]) {
     // Send the message to the receiver socket of length 200
-    int byte_count = send(reciever_socket, message, 200, 0);
+    const int byte_count = send(receiver_socket, message, 200, 0);
 
     // Error checking 
     if (byte_count == SOCKET_ERROR) {
@@ -40,11 +37,11 @@ int Client::send_msg(const SOCKET reciever_socket, const char message[]) {
     return 0;
 }
 
-int Client::receiver(SOCKET socket) {
+int Client::receiver() const {
     while (true) {
         char buffer[200]; // buffer to write to
         // wait for a message from the server
-        int byte_count = recv(connection_socket, buffer, sizeof(buffer), 0);
+        const int byte_count = recv(connection_socket_, buffer, sizeof(buffer), 0);
 
         // Error checking
         if (byte_count < 0) {
@@ -52,8 +49,7 @@ int Client::receiver(SOCKET socket) {
             continue;
         }
 
-        // Ouput to client's console
+        // Output to client's console
         std::cout << buffer << std::endl;
     }
-    return 0;
 }
