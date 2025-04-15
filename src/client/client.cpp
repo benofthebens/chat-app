86c472821a3d9086dbb22d56013eb3425c0e6e3b
benjamin whalley <benjaminjwhalley@outlook.com>
2025-04-15 18:26:23 +0100

    chore: added comments

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,42 +1,58 @@
 #include "../../include/client.h"
 #include <iostream>
 #include <thread>
+#include <array>
 
 int Client::start() {
-   if (connect(connection_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+    
+    // Connect to the connection_socket with the service
+    if (connect(connection_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
         std::cout << "Failed to conenct" << std::endl;
         return -1;
-   }
-   while (true) {
+    }
+
+    while (true) {
+        // Create buffer with 200 size to get message
         char buffer[200];
 
+        // Create receiver thread to get response from server
         std::thread receiver(&Client::receiver, this, connection_socket);
-        receiver.detach();
+        receiver.detach(); // detach
 
+        // Read message from cli
         std::cin.getline(buffer, 200);
         send_msg(connection_socket, buffer);
-   }
+    }
+
     return 0;
 }
-int Client::send_msg(const SOCKET reciever_socket, const char message[])  {
 
-    int byte_count = send(reciever_socket, message, sizeof(message), 0);
+int Client::send_msg(const SOCKET reciever_socket, const char message[]) {
+    // Send the message to the receiver socket of length 200
+    int byte_count = send(reciever_socket, message, 200, 0);
 
+    // Error checking 
     if (byte_count == SOCKET_ERROR) {
         std::cout << "Unable to send message" << std::endl;
         return -1;
     }
 
+    return 0;
 }
 
 int Client::receiver(SOCKET socket) {
     while (true) {
-        char buffer[200];
+        char buffer[200]; // buffer to write to
+        // wait for a message from the server
         int byte_count = recv(connection_socket, buffer, sizeof(buffer), 0);
+
+        // Error checking
         if (byte_count < 0) {
             std::cout << "Failed to get data" << std::endl;
             continue;
         }
+
+        // Ouput to client's console
         std::cout << buffer << std::endl;
     }
     return 0;
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -5,14 +5,19 @@
 
 void Server::receive(SOCKET socket) {
     while (true) {
+        // buffer to be read into
         char buffer[200];
+        // Waits for the client to send a request
         int byte_count = recv(socket, buffer, sizeof(buffer), 0);
 
         if (byte_count < 0) {
             std::cout << "Failed to get data" << std::endl;
             continue;
         }
-        std::cout << buffer << std::endl;
+
+        std::cout << byte_count << std::endl;
+
+        // Broadcast message to everyone except the client sent
         for (SOCKET client : clients) {
             if (client == socket) continue;
             this->send_msg(client, buffer);
@@ -22,39 +27,48 @@ void Server::receive(SOCKET socket) {
 
 int Server::start() {
 
+    // Bind the connection socket to the service
     if (bind(connection_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
         std::cout << "Failed to bind socket" << std::endl;
         return -1;
     }
+
+    // Listen on the the binded socket
     if (listen(connection_socket, 1) == SOCKET_ERROR) {
         std::cout << "Failed to listen on the socket" << std::endl;
         return -1;
     }
+
     std::cout << "Server Running on the port on " << port << std::endl;
 
     SOCKET accept_socket;
     while (true) {
+
+        // Waits for accept of connections
         accept_socket = accept(this->connection_socket, nullptr, nullptr);
         if (accept_socket == INVALID_SOCKET) {
             std::cout << "failed to accept" << std::endl;
             continue;
         }
 
+        // put it in the list of clients connected
         clients.push_back(accept_socket);
 
+        // Create thread per user connected 
         std::thread receive(&Server::receive, this, accept_socket);
-        receive.detach();
+        receive.detach(); // detaches the thread
     }
 }
 
 int Server::send_msg(const SOCKET receiver_socket, const char message[]) {
-    std::cout << sizeof(char) << std::endl;
-    int byte_count = send(receiver_socket, message, sizeof(message), 0);
+    // Send a message to the receiver socket with a length of 200
+    int byte_count = send(receiver_socket, message, 200, 0);
     if (byte_count == SOCKET_ERROR ) return -1;
     return 0;
 }
 
 int Server::broardcast_message(const char message[]) {
+    // Send message to every client
     for (SOCKET client : clients) {
         this->send_msg(client, message);
     }
