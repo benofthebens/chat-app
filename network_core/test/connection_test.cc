783ed42ce6385e6c840bda328b21baa6477216c3
benjamin whalley <benjaminjwhalley@outlook.com>
2025-12-25 00:27:43 +0000

    tests: added unit tests

---
+++ network_core/test/connection_test.cc
@@ -0,0 +1,146 @@
+#include "network_core/connection.h"
+
+#include <gtest/gtest.h>
+
+class ConnectionTest : public ::testing::Test {
+protected:
+    std::unique_ptr<ServerConnection> server_;
+    std::unique_ptr<ClientConnection> client_;
+
+    void SetUp() override {
+        constexpr WORD version_requested = MAKEWORD(2, 2); // gets the version requested
+        WSADATA wsa_data;
+        WSAStartup(version_requested, &wsa_data);
+
+        server_ = std::make_unique<ServerConnection>();
+        client_ = std::make_unique<ClientConnection>();
+    }
+
+    void TearDown() override {
+        WSACleanup();
+    }
+};
+
+TEST_F(ConnectionTest, Server_BindValidSocket) {
+    const Socket socket = { "127.0.0.1", 8080 };
+    const int result = server_->Bind(socket);
+    EXPECT_NE(result, -1);
+}
+
+TEST_F(ConnectionTest, Server_BindDuplicateSocket) {
+    const Socket socket = { "127.0.0.1", 8080 };
+    const int result1 = server_->Bind(socket);
+    EXPECT_NE(result1, -1);
+    const int result2 = server_->Bind(socket);
+    EXPECT_EQ(result2, -1);
+}
+
+TEST_F(ConnectionTest, Server_BindInvalidSocket) {
+    const Socket socket = { "INVALID", 8080 };
+    const int result = server_->Bind(socket);
+    EXPECT_EQ(result, -1);
+}
+
+TEST_F(ConnectionTest, Server_ListenValidSocket) {
+    const Socket socket = { "127.0.0.1", 8080 };
+
+    const int bind_result = server_->Bind(socket);
+    EXPECT_NE(bind_result, -1);
+
+    const int listen_result = server_->Listen();
+    EXPECT_NE(listen_result, SOCKET_ERROR);
+    EXPECT_TRUE(server_->IsListening());
+}
+
+TEST_F(ConnectionTest, Server_ListenInvalidSocket) {
+    const Socket socket = { "INVALID", 8080 };
+
+    const int bind_result = server_->Bind(socket);
+    EXPECT_EQ(bind_result, -1);
+
+    const int listen_result = server_->Listen();
+    EXPECT_EQ(listen_result, SOCKET_ERROR);
+    EXPECT_FALSE(server_->IsListening());
+}
+
+TEST_F(ConnectionTest, Server_ListenUnboundSocket) {
+    const Socket socket = { "127.0.0.1", 8080 };
+
+    const int listen_result = server_->Listen();
+    EXPECT_EQ(listen_result, SOCKET_NOT_BOUND);
+    EXPECT_FALSE(server_->IsListening());
+}
+
+TEST_F(ConnectionTest, Client_ConnectValidSocket) {
+    const Socket socket = { "127.0.0.1", 8080 };
+
+    const int bind_result = server_->Bind(socket);
+    EXPECT_NE(bind_result, -1);
+
+    const int listen_result = server_->Listen();
+    EXPECT_NE(listen_result, SOCKET_ERROR);
+    EXPECT_TRUE(server_->IsListening());
+
+    const int conn_result = client_->Connect(socket);
+    EXPECT_NE(conn_result, SOCKET_ERROR);
+
+    const auto client = server_->Accept();
+    EXPECT_NE(client, nullptr);
+    EXPECT_TRUE(client_->IsConnected());
+}
+
+TEST_F(ConnectionTest, Client_ConnectInvalidSocket) {
+    const Socket socket = { "INVALID", 8080 };
+
+    const int bind_result = server_->Bind(socket);
+    EXPECT_EQ(bind_result, -1);
+
+    const int listen_result = server_->Listen();
+    EXPECT_EQ(listen_result, SOCKET_ERROR);
+    EXPECT_FALSE(server_->IsListening());
+
+    const int conn_result = client_->Connect(socket);
+    EXPECT_EQ(conn_result, SOCKET_ERROR);
+
+    const auto client = server_->Accept();
+    EXPECT_EQ(client, nullptr);
+    EXPECT_FALSE(client_->IsConnected());
+}
+
+TEST_F(ConnectionTest, Client_SendOnValidConnection) {
+    const Socket socket = { "127.0.0.1", 8080 };
+
+    const int bind_result = server_->Bind(socket);
+    EXPECT_NE(bind_result, -1);
+
+    const int listen_result = server_->Listen();
+    EXPECT_NE(listen_result, SOCKET_ERROR);
+    EXPECT_TRUE(server_->IsListening());
+
+    const int conn_result = client_->Connect(socket);
+    EXPECT_NE(conn_result, SOCKET_ERROR);
+
+    const auto client = server_->Accept();
+    EXPECT_NE(client, nullptr);
+    EXPECT_TRUE(client_->IsConnected());
+
+    const std::string message = "Hello World";
+    const int n = static_cast<int>(message.length());
+
+    const int send_result = client_->Send(message.data(), n);
+    ASSERT_EQ(send_result, n);
+
+    std::string buffer(message.length(), '\0');
+    const int receive_result = client->Receive(buffer.data(), n);
+    EXPECT_EQ(receive_result, n);
+    EXPECT_EQ(buffer, message);
+}
+
+TEST_F(ConnectionTest, Client_SendInvalidConnetion) {
+    const auto* test_data = "TEST";
+    const int n = static_cast<int>(strlen(test_data));
+
+    const int result = client_->Send(test_data, n);
+    EXPECT_EQ(result, SOCKET_NOT_BOUND);
+}
+
---
+++ network_core/test/socket_test.cc
@@ -0,0 +1,110 @@
+#include <gtest/gtest.h>
+#include "network_core/socket.h"
+
+class TcpSocketTest : public ::testing::Test {
+protected:
+    std::unique_ptr<NetworkSocket> tcp_socket_;
+    void SetUp() override {
+        constexpr WORD version_requested = MAKEWORD(2, 2); // gets the version requested
+        WSADATA wsa_data;
+        WSAStartup(version_requested, &wsa_data);
+
+        tcp_socket_ = std::make_unique<NetworkSocket>();
+    }
+    void TearDown() override {
+        WSACleanup();
+    }
+};
+
+TEST_F(TcpSocketTest, ConstructorCreatesValidHandle) {
+    EXPECT_NE(tcp_socket_->GetHandle(), INVALID_SOCKET);
+}
+
+TEST_F(TcpSocketTest, BindValidSocket) {
+    Socket socket{ "127.0.0.1", 8080 };
+    int result = tcp_socket_->Bind(socket);
+    EXPECT_NE(result, SOCKET_ERROR);
+}
+
+TEST_F(TcpSocketTest, BindInvalidSocket) {
+    Socket socket{ "INVALID", 8080 };
+    int result = tcp_socket_->Bind(socket);
+    EXPECT_EQ(result, SOCKET_ERROR);
+}
+
+TEST_F(TcpSocketTest, ListenValidSocket) {
+    Socket socket{ "127.0.0.1", 8080 };
+    const int bind_result = tcp_socket_->Bind(socket);
+    EXPECT_NE(bind_result, SOCKET_ERROR);
+    const int listen_result = tcp_socket_->Listen();
+    EXPECT_NE(listen_result, SOCKET_ERROR);
+}
+
+TEST_F(TcpSocketTest, ListenInvalidSocket) {
+    Socket socket{ "INVALID", 8080 };
+    const int bind_result = tcp_socket_->Bind(socket);
+    EXPECT_EQ(bind_result, SOCKET_ERROR);
+    const int listen_result = tcp_socket_->Listen();
+    EXPECT_EQ(listen_result, SOCKET_ERROR);
+}
+
+TEST_F(TcpSocketTest, ConnectValidSocket) {
+    Socket socket{ "127.0.0.1", 8080 };
+
+    const int bind_result = tcp_socket_->Bind(socket);
+    EXPECT_NE(bind_result, SOCKET_ERROR);
+
+    const int listen_result = tcp_socket_->Listen();
+    EXPECT_NE(listen_result, SOCKET_ERROR);
+
+    NetworkSocket client_socket;
+    const int conn_result = client_socket.Connect(socket);
+    EXPECT_NE(conn_result, SOCKET_ERROR);
+
+    const auto client = tcp_socket_->Accept();
+    EXPECT_NE(client, nullptr);
+}
+
+TEST_F(TcpSocketTest, ConnectInvalidSocket) {
+    const Socket socket{ "INVALID", 8080 };
+
+    const int bind_result = tcp_socket_->Bind(socket);
+    EXPECT_EQ(bind_result, SOCKET_ERROR);
+
+    const int listen_result = tcp_socket_->Listen();
+    EXPECT_EQ(listen_result, SOCKET_ERROR);
+
+    NetworkSocket client_socket;
+    int conn_result = client_socket.Connect(socket);
+    EXPECT_EQ(conn_result, SOCKET_ERROR);
+
+    const auto client = tcp_socket_->Accept();
+    EXPECT_EQ(client, nullptr);
+}
+
+TEST_F(TcpSocketTest, SendMessageOverConnection) {
+    const Socket socket{ "127.0.0.1", 8080 };
+
+    const int bind_result = tcp_socket_->Bind(socket);
+    EXPECT_NE(bind_result, SOCKET_ERROR);
+
+    const int listen_result = tcp_socket_->Listen();
+    EXPECT_NE(listen_result, SOCKET_ERROR);
+
+    NetworkSocket client_socket;
+    const int conn_result = client_socket.Connect(socket);
+    EXPECT_NE(conn_result, SOCKET_ERROR);
+
+    const auto stream = tcp_socket_->Accept();
+    EXPECT_NE(stream->GetHandle(), INVALID_SOCKET);
+
+    const std::string message = "Hello World!";
+    const int n = static_cast<int>(message.length());
+    const int send_result = client_socket.Send(message.data(), n);
+    EXPECT_EQ(send_result, message.length());
+
+    std::string buffer(message.length(), '\0');
+    const int receive_result = stream->Receive(buffer.data(), n);
+    EXPECT_EQ(receive_result, message.length());
+    EXPECT_EQ(buffer, message);
+}
